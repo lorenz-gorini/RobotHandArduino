9a9740c96a08ad4805fde8e13bd4c96cdb621957
lorenz-gorini <lore.gorini@gmail.com>
2019-09-20 22:59:32 +0200

    thread example and comments for future threads

--- lib/training_phase/collect_training_dataset.py
+++ lib/training_phase/collect_training_dataset.py
@@ -3,7 +3,7 @@ This will generate the dataset for the ML training
 For every label we will record the data and put it in a separate folder depending on its label
 (watch code from: https://youtu.be/qbW6FRbaSl0?t=199 )
 """
-from watchdog.observers import read_directory_changes
+from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 import os
@@ -11,11 +11,13 @@ import json
 import time
 
 
-DATA_COLLECTING_FOLDER = ".\\" # os.path.join(LIB_FOLDER,".\\")
+DATA_COLLECTING_FOLDER = ".\\data_collecting_folder\\" # os.path.join(LIB_FOLDER,".\\")
 
 class MyHandler(FileSystemEventHandler):
     i=1
     def on_modified(self, event):
         new_name = "test"
+        folder_destination = os.path.join(DATA_COLLECTING_FOLDER,f"{label}\\")
         for filename in os.listdir(DATA_COLLECTING_FOLDER):
-            # Rename the file
+            # Rename the file to a timestamp (we care only about the label that will be in the folder name)
+
