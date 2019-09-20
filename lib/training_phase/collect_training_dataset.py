544c6452a184bbbd5209e55027af229e3dead116
lorenz-gorini <lore.gorini@gmail.com>
2019-09-20 23:06:56 +0200

    thread example and comments for future threads

---
+++ lib/training_phase/collect_training_dataset.py
@@ -0,0 +1,7 @@
+"""
+This will generate the dataset for the ML training
+For every label we will record the data and put it in a separate folder depending on its label
+(watch code from: https://youtu.be/qbW6FRbaSl0?t=199 )
+"""
+
+# TODO: INSTEAD, WE JUST COLLECT THE DATA AND STORE THEM IN THE CORRECT FOLDER! \\dataset\\{label}
