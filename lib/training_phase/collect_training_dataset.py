632a629778a848a355279917775cb395f4f5a013
lorenz-gorini <lore.gorini@gmail.com>
2019-09-20 22:53:03 +0200

    thread example and comments for future threads

--- lib/training_phase/collect_training_dataset.py
+++ lib/training_phase/collect_training_dataset.py
@@ -3,3 +3,19 @@ This will generate the dataset for the ML training
 For every label we will record the data and put it in a separate folder depending on its label
 (watch code from: https://youtu.be/qbW6FRbaSl0?t=199 )
 """
+from watchdog.observers import read_directory_changes
+from watchdog.events import FileSystemEventHandler
+
+import os
+import json
+import time
+
+
+DATA_COLLECTING_FOLDER = ".\\" # os.path.join(LIB_FOLDER,".\\")
+
+class MyHandler(FileSystemEventHandler):
+    i=1
+    def on_modified(self, event):
+        new_name = "test"
+        for filename in os.listdir(DATA_COLLECTING_FOLDER):
+            # Rename the file
