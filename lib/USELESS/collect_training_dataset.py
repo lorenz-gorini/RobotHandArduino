cec417cf74fbd543909f0fde70a72400f121caa0
lorenz-gorini <lore.gorini@gmail.com>
2019-09-20 23:04:46 +0200

    thread example and comments for future threads

--- lib/USELESS/collect_training_dataset.py
+++ lib/USELESS/collect_training_dataset.py
@@ -16,8 +16,11 @@ DATA_COLLECTING_FOLDER = ".\\data_collecting_folder\\" # os.path.join(LIB_FOLDER
 class MyHandler(FileSystemEventHandler):
     i=1
     def on_modified(self, event):
-        new_name = "test"
+        new_name = f"spectrum_{time.time_ns()}"
         folder_destination = os.path.join(DATA_COLLECTING_FOLDER,f"{label}\\")
         for filename in os.listdir(DATA_COLLECTING_FOLDER):
             # Rename the file to a timestamp (we care only about the label that will be in the folder name)
+            source_file = DATA_COLLECTING_FOLDER + "\\" + filename
+            os.rename(source_file, folder_destination+"\\"+new_name)
 
+# To complete, see youtube video above
