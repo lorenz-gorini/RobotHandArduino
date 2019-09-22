eb7ca735f68e3f6c0373134c39f433ff6dcec630
lorenz-gorini <lore.gorini@gmail.com>
2019-09-23 00:16:59 +0200

    thread example and comments for future threads

--- lib/training_phase/collect_training_dataset.py
+++ lib/training_phase/collect_training_dataset.py
@@ -1,7 +1,40 @@
 """
 This will generate the dataset for the ML training
 For every label we will record the data and put it in a separate folder depending on its label
-(watch code from: https://youtu.be/qbW6FRbaSl0?t=199 )
 """
 
 # TODO: INSTEAD, WE JUST COLLECT THE DATA AND STORE THEM IN THE CORRECT FOLDER! \\dataset\\{label}
+from enum import Enum
+
+
+class Fingers(Enum):
+    thumb = 0,
+    index = 1
+    middle = 2
+    ring = 3
+    pinky = 4
+    all = 5
+
+class Action(Enum):
+    close = 0
+    open = 1
+
+class TrainingDataset:
+
+    def __init__(self, action: str, finger: str):
+        self.action = action
+        self.finger = finger
+        self.label = Action.
+    def collect(self):
+        print(f"Lift the {self.finger_num}")
+    def categorize(self, label):
+        pass
+
+class TrainingFingers(TrainingDataset):
+    def __init__(self, action_label):
+        super().__init__(action_label)
+
+class TrainingHand(TrainingDataset):
+    pass
+
+class
