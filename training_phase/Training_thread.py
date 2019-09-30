64b9a362d532b9a0a7204f79c9557f1b73956589
lorenz-gorini <lore.gorini@gmail.com>
2019-09-30 13:48:48 +0200

    Draft of the process and queues!

--- training_phase/Training_thread.py
+++ training_phase/Training_thread.py
@@ -1,4 +1,6 @@
-from multiprocessing import Queue, Process, Value
+import ctypes
+import multiprocessing as mp
+# from multiprocessing import Queue, Process, Value
 
 from lib.GestureLabels import action_ids, moving_part_ids, GestureLabels
 from lib.frequency_spectrum.frequency_spectrum import generate_spectra
@@ -6,22 +8,24 @@ from lib.retrieve_data.retrieve_data_socket import push_random_data
 
 def start_training():
 
-    stop_input = Value('i')
+    stop_input = mp.Value('i')
+    is_training = mp.Value('b')
     stop_input.value = 0
     # stored_data_batches = Array('d', 20)
-    stored_data_batches = Queue()
-    stored_spectrum_batches = Queue()
+    stored_data_batches = mp.Queue()
+    stored_spectrum_batches = mp.Queue()
     for train_action in action_ids:
         for train_moving_part in moving_part_ids:
             # GestureLabels.get_gesture_label(train_action,train_moving_part)
-            # TODO Uncomment this when connecting the sccket
+            # TODO Uncomment this when connecting the socket
             # retrieve_data_process = Process(target=raw_data.store_data, args=(stored_data_batches, stop_input))
-            retrieve_data_process = Process(target=push_random_data, args=(stored_data_batches, stop_input))
-            analyze_data_process = Process(target=generate_spectra, args=(stored_data_batches, stored_spectrum_batches,
+            retrieve_data_process = mp.Process(target=push_random_data, args=(stored_data_batches, stop_input))
+            analyze_data_process = mp.Process(target=generate_spectra, args=(stored_data_batches,
+                                                                            stored_spectrum_batches,
                                                                           stop_input))
-            start_training_process = Process(target=start_training, args=(stored_spectrum_batches, stop_input,
+            start_training_process = mp.Process(target=start_training, args=(stored_spectrum_batches, stop_input,
                                                                           train_action, train_moving_part))
-            # TODO Insert a booloan to distiguish is_collecting ("is_labeling"/"controlling" otherwise) or not.
+            # TODO Insert a boolean to distinguish is_collecting ("is_labeling"/"controlling" otherwise) or not.
             #  In that case it has to stop and register data (is it really different???)
             retrieve_data_process.start()
             analyze_data_process.start()
